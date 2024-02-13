42fe7e11002b94ac92f6b70427040c8a8e270fb6
Anthoonyyy <Anthony212007@hotmail.com>
2024-02-13 22:12:24 +0100

    finalisation du formulaire

--- script.js
+++ script.js
@@ -1,37 +1,42 @@
 const formContainer = document.querySelector("#formContainer");
 
-formContainer.addEventListener('submit', function(event) {
-    event.preventDefault();});
+formContainer.addEventListener('submit', function (event) {
+    event.preventDefault();
+});
 
-    function checkAge() {
-        var ageInput = document.getElementById('ageInput').value;
-        var resultDiv = document.getElementById('result');
-       
-        if (ageInput >= 20) {
-            resultDiv.textContent = "Vous avez 20 ans ou plus ! Redirection en cours...";
-            resultDiv.style.color = "green";
-            setTimeout(function () {
-                window.location.href = "index.html";
-            }, 1300);
-        } else {
-            resultDiv.textContent = "Vous n'avez pas l'âge requis.";
-            resultDiv.style.color = "red";
-        }
+function checkAge() {
+    var ageInput = document.getElementById('ageInput').value;
+    var resultDiv = document.getElementById('result');
 
-        resultDiv.style.display = "block";
+    if (ageInput >= 20) {
+        resultDiv.textContent = "Vous avez 20 ans ou plus ! Redirection en cours...";
+        resultDiv.style.color = "green";
+        setTimeout(function () {
+            window.location.href = "index2.html";
+        }, 1300);
+    } else {
+        resultDiv.textContent = "Vous n'avez pas l'âge requis.";
+        resultDiv.style.color = "red";
     }
+
+    resultDiv.style.display = "block";
+}
 
 var prénom = document.getElementById("prenom").value;
 
 function submitForm() {
     var resultDiv = document.getElementById('resultat');
-    var password = document.getElementById('mdp').value;    // idem  ligne 7
-
+    var password = document.getElementById('mdp').value;
+    var prenom = document.getElementById("prenom").value;
+    var messageAlerte = "Bienvenue  " + prenom;
     if (password.length >= 10 && /[A-Z]/.test(password) && /\d/.test(password) && /[!@#$%^&*(),.?":{}|<>'-+/]/.test(password)) {
         resultDiv.textContent = "Inscription réussie !";
         resultDiv.style.color = "green";
+        resultDiv.style.textAlign = "center";
+        resultDiv.style.marginTop = "20px";
+        alert(messageAlerte);
         setTimeout(function () {
-            window.location.href = "https://2023.webdev-cf2m.be/Anthony/Siteprefo/"; 
+            window.location.href = "https://2023.webdev-cf2m.be/Anthony/Siteprefo/";
         }, 3000);
     } else {
         resultDiv.textContent = "Veuillez remplir tous les critères du mot de passe.";
@@ -39,11 +44,13 @@ function submitForm() {
     }
 }
 //Créer une alerte//
-e=true;
 function ShowHide() {
-    if(e){
-        document.getElementById("mdp").setAttribute("type","text");
-        e=false;
+    const passwordInput = document.getElementById("mdp");
+
+    if (passwordInput.type === "password") {   //Si le mdp est de type password
+        passwordInput.type = "text";           //on le change en texte
+    } else {
+        passwordInput.type = "password";       //Sinon on le remet en type password
     }
-    
-  }
+}
+
